f78fcd06136ca67d704b08de08412e5400cac4db
Anpoli13 <anpoli13@gmail.com>
2020-04-02 13:00:33 +0300

    add nav-active function

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+// Open overlay
 const overlay = document.querySelector('.overlay')
 
 function myFunction(x) {
-    x.classList.toggle("change");
+    x.classList.toggle('change');
     overlay.classList.toggle('hide');
-
   }
+
+// Applying active class to navigation
+
+const navBtns = document.getElementsByClassName('nav');
+
+for (let i=0; i<navBtns.length; i+=1) {
+  navBtns[i].addEventListener('click', function() {
+    let current = document.getElementsByClassName('nav-active');
+    current[0].className = current[0].className.replace(' nav-active', '');
+    this.className += ' nav-active';
+  });
+}
